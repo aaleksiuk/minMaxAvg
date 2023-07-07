7c7203449b93a00ee60e94e9c2cb1ad73520109a
Anna Aleksiuk <aal@scalepoint.com>
2023-07-07 10:45:12 +0200

    Add project files.

---
+++ minMaxAvg/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.ExceptionServices;
+
+
+int[] numbers = InitializeTable();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = GetValue(i);
+}
+
+int minNumber = numbers.Min();
+int maxNumber = numbers.Max();
+double average = numbers.Average();
+
+Console.WriteLine($"Maximum number is: {maxNumber}\nMinimum number is: {minNumber}\nAverage number is: {average}");
+
+int[] InitializeTable()
+{
+    int lenght;
+    try
+    {
+        Console.WriteLine("Quantity of numbers from user:");
+        lenght = Convert.ToInt32(Console.ReadLine());
+        if (lenght >= 1)
+        {
+            return new int[lenght];
+        }
+        else
+        {
+            Console.WriteLine("Quantity of numbers from user MUST be greater than 0");
+            return InitializeTable();
+        }
+    }
+
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        return InitializeTable();
+    }
+
+}
+int GetValue(int i)
+{
+    try
+    {
+        Console.WriteLine($"Get {i} element");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        return GetValue(i);
+    }
+}
